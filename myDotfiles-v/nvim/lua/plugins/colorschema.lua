677ee997d552d5368ed2a6cd504b455b7228d9af
venkatesh2100 <venkyvenkat.iit@gmail.com>
2025-06-08 22:23:40 +0530

    configs

---
+++ myDotfiles-v/nvim/lua/plugins/colorschema.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    -- "craftzdog/solarized-osaka.nvim",
+    "folke/tokyonight.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+}
